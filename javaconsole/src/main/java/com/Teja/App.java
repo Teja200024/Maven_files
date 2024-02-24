017bee213bacd291be47376f5bef304cd13f14d7
root <root@ip-172-31-24-71.ec2.internal>
2024-02-24 11:24:43 +0000

    Maven 1

---
+++ javaconsole/src/main/java/com/Teja/App.java
@@ -0,0 +1,18 @@
+package com.Teja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int a=23;
+	int b=2567;
+	int c;
+	c=a+b;
+	System.out.println("addition is"+c);
+        System.out.println( "Hello World!" );
+    }
+}
